05d3ac3fded0a8d8aa69e1cddb4e56daffa9ba2a
Niccolo Delfino <delfino.nicco@libero.it>
2020-12-17 15:23:24 +0100

    prompt, array e push ok

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Chiedere all’utente il cognome
+// inserirlo in un array con altri cognomi: ‘Bianchi’, ‘Rossi’, ‘Duzioni’, ‘Balsano’, ‘Verdi’
+// stampa la lista ordinata alfabeticamente (occhio alle maiuscole, se volete potete convertire il nome prima inserirlo).
+// Scrivi anche la posizione "umana" (quindi per come contiamo noi, non l'indice dell'array) della lista in cui il nuovo utente si trova
+
+// chiedo il cognome
+var chiediCognome = prompt('Inserisci qui il tuo cognome');
+console.log(chiediCognome);
+
+// creo array
+var cognomi = ['Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Verdi'];
+console.log(cognomi);
+
+// inserico il cognome del prompt nell'array
+cognomi.push(chiediCognome);
+console.log(cognomi);
